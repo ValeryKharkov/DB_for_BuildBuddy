61dc32a4a7819233316d2f63055a458ca2250c45
ValeryKharkov <kharkov.valery@gmail.com>
2023-04-06 13:04:07 +0300

    create table objects

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ import sqlite3 as sl
 
 # открытие файла с базой данных
 con = sl.connect('build_buddy.db')
+
+# создание таблицы
+with con:
+    con.execute("""
+        CREATE TABLE objects (
+        id integer primary key,
+        cadastral_number integer primary key,
+        addres varchar(20),
+        problem_object integer,
+        document_link varchar(20),
+        foto_link varchar(20),
+        status integer,
+        tracking integer,
+        priority varchar(20),
+        deadline date
+        );
+    """)
