555468bf4ee8dc4ad123da5d5ef5e60e6b14f3c4
ValeryKharkov <kharkov.valery@gmail.com>
2023-04-09 22:55:10 +0300

    Определена функция: поиск объекта

--- db_build_buddy.py
+++ db_build_buddy.py
@@ -23,54 +23,34 @@ cur.execute(""" CREATE TABLE IF NOT EXISTS objects(
 con.commit()
 
 
-# Добавление данных в таблицу (хардкод)
-# data = [
-#     ('Кондратьевский просп., 44', '...', '...'),
-#     ('Полюстровский просп., 18', '...', '...')
-# ]
-#
-# cur.executemany("""INSERT OR IGNORE INTO objects VALUES (?, ?, ?, ?);""", data)
-# con.commit()
-
-
-# Добавление данных через функцию def db_table_val(id: int, cadastral_number: int, addres: str, problem_object: int,
-# document: str, foto: str, status: int, tracking: int, priority: str, deadline: str): cur.execute("""INSERT INTO
-# objects ( id, cadastral_number, addres, problem_object, document_link, foto_link, status, tracking, priority,
-# deadline) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", (id, cadastral_number, addres, problem_object, document, foto,
-# status, tracking, priority, deadline)) con.commit()
-
 # Функция: поиск объекта
 def search_object(text, search_type):
     object_info = []  # готовим выходное представление
-    sql = ''  # инициализируем базу данных
+    # инициализируем базу данных
+    connect = sqlite3.connect('db_build_buddy.db')
+    cursor = connect.cursor()
+    print("Подключен к SQLite")
     # здесь поиск по базе
     if search_type == 'номер':
-        pass  # алгоритм поиска по кадастровому номеру в БД
+        # алгоритм поиска по кадастровому номеру в БД
+        sql_query = """select * from objects where cadastral_number = ?"""
+        cursor.execute(sql_query, (text,))
+        records = cursor.fetchall()
+        print(records)
+
     elif search_type == 'адрес':
-        pass  # алгоритм поиска по адресу в БД
-    if text in sql:  # готовим нужный вариант вывода, если text в базе
-        object_info.append(0)
-        object_info.append('данные из БД')
-    else:  # если в БД не находится нужный объект, выдаём код ошибки
-        object_info.append(1)
-    return object_info
+        # алгоритм поиска по адресу в БД
+        sql_query = """select * from objects where addres = ?"""
+        cursor.execute(sql_query, (text,))
+        records = cursor.fetchall()
+        print(records)
+        cursor.close()
 
 
-# Функция: добавить объект
-def add_object(arg_addres: int):
-    con = sqlite3.connect('db_build_buddy.db')
-    cur = con.cursor()
-    data = []
-    data.append(arg_addres)
-    cur.execute("""INSERT OR IGNORE INTO objects(addres) VALUES(?)""", data)
-    con.commit()
-    cur.close()
 
 
-# Функция: просмотр добавленных объектов
-def view_object():
-    pass
-
+search_object('84141203001815', 'номер')
+search_object('Проезд Смольный, д. 1, лит. Б.', 'адрес')
 
 # Функция: конвертация изображений и файлов в бинарные данные
 def convert_to_binary_data(filename):
@@ -83,7 +63,7 @@ def convert_to_binary_data(filename):
 # Функция: добавить данные в таблицу
 def insert_data_to_the_table(arg_cadastral_number, arg_addres, arg_status_problem, arg_document, arg_photo,
                              arg_status_execution, arg_tracking, arg_priority, arg_deadline):
-    global sqlite_connection
+
     try:
         sqlite_connection = sqlite3.connect('db_build_buddy.db')
         cursor = sqlite_connection.cursor()
@@ -111,7 +91,7 @@ def insert_data_to_the_table(arg_cadastral_number, arg_addres, arg_status_proble
             print("Соединение с SQLite закрыто")
 
 
-# Тестирование: добавление blob файлов (хардкод)
+# Тестирование: добавление данных в таблицу (хардкод)
 insert_data_to_the_table(47141203001814, 'Проезд Смольный, д. 1, лит. Б.', 1, 'blob_files/document_1.docx',
                          'blob_files/photo_1.jpg', 1, 1, 1, '24.05.2023')
 insert_data_to_the_table(84141203001815, 'Лахтинский проспект, 2 к3 ст1', 0, 'blob_files/document_2.docx',
